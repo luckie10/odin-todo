2e3048997f4145188eb0a7585129fd41e1b47f11
Boban Kilvin <btrajcevski10@gmail.com>
2023-03-22 04:44:16 -0400

    add createElement function to util

--- src/util.js
+++ src/util.js
@@ -12,4 +12,27 @@ const stateFunctions = (state) => ({
   },
 });
 
-export { stateFunctions };
+/**
+ * Create an HTML element with any attribute
+ *
+ * @param {string} tagName - Name of HTML tag
+ * @param {{name: string, value: string}} attributes - Object of attributes to be appened to HTML element
+ * @returns {Element} HTML Element
+ */
+function createElement(tagName, attributes) {
+  const element = document.createElement(tagName);
+
+  if (attributes) {
+    Object.entries(attributes).map(([name, value]) => {
+      if (name == "textContent") {
+        element.textContent = value;
+      } else {
+        element.setAttribute(name, value);
+      }
+    });
+  }
+
+  return element;
+}
+
+export { stateFunctions, createElement };
