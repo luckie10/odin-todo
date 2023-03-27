bfb6a1d82ca8d09b22d40e69a20451854e069e33
Boban Kilvin <btrajcevski10@gmail.com>
2023-03-27 02:06:23 -0400

    Add complete boolean to Task

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,7 +1,7 @@
 import { stateFunctions } from "../util.js";
 
 const Task = (name, desc, dueDate, prio, projectName) => {
-  const state = { name, desc, dueDate, prio, projectName };
+  const state = { name, desc, dueDate, prio, projectName, complete: false };
 
   return { ...stateFunctions(state) };
 };
